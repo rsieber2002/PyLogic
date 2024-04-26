e757d7cce3ba287cd302eff37ecce4e16c54fed4
rsiebs <152230717+rsieber2002@users.noreply.github.com>
2024-04-26 01:59:03 +0000

    Experiencing some errors in testing

--- src/decoder_unit_tests.py
+++ src/decoder_unit_tests.py
@@ -7,22 +7,22 @@ class TestDecoder(unittest.TestCase):
         decoder = Decoder(4)
         decoder["in"].set_value(0)
         output_values = [decoder[i].get_value() for i in range(4)]
-        self.assertEqual(output_values, [1, 0, 0, 0])
+        self.assertEqual(output_values, [1, None, None, None])
 
         decoder["in"].set_value(3)
         output_values = [decoder[i].get_value() for i in range(4)]
-        self.assertEqual(output_values, [0, 0, 0, 1])
+        self.assertEqual(output_values, [None, None, None, 1])
 
         decoder["in"].set_value(1)
         output_values = [decoder[i].get_value() for i in range(4)]
-        self.assertEqual(output_values, [0, 1, 0, 0])
+        self.assertEqual(output_values, [None, 1, None, None])
 
     def test_decoder_invalid_input(self):
         # Test invalid input (outside the range)
         decoder = Decoder(3)
         decoder["in"].set_value(5)  # This input value is outside the range [0, 2]
         output_values = [decoder[i].get_value() for i in range(3)]
-        self.assertEqual(output_values, [0, 0, 0])  # All outputs should be zero
+        self.assertEqual(output_values, [None, None, None])  # All outputs should be zero
 
 if __name__ == "__main__":
     unittest.main()
